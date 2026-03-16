4df0d41b9baaa575df8cdf69c1e1314d6b8b4623
leydy <leydymayumy@gmail.com>
2026-03-16 15:19:58 -0500

    Realizar iteraciones de elementos

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,6 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+interface Character{
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   templateUrl: './dragonball-page.component.html',
 })
-export class DragonballPageComponent {}
+export class DragonballPageComponent {
+  characters = signal<Character[]>([
+    {
+      id: 1,
+      name: 'Goku',
+      power: 9001,
+    },
+    {
+      id: 2,
+      name: 'Vegeta',
+      power: 8000,
+    },
+    {
+      id: 3,
+      name: 'Picoro',
+      power: 3000,
+    },
+  ]);
+}
